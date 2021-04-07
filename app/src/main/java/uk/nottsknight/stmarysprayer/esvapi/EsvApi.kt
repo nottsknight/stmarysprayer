5f47d879f0ba4fc582c87e097a372b749c62b16d
Ian Knight <ian.knight.1990@gmail.com>
2021-04-07 13:04:55 +0100

    Empty EsvApi class

---
+++ app/src/main/java/uk/nottsknight/stmarysprayer/esvapi/EsvApi.kt
@@ -0,0 +1,6 @@
+package uk.nottsknight.stmarysprayer.esvapi
+
+object EsvApi {
+    private val API_KEY = "8c26a895d2ac3ef9b216ce33534e7566af088bf4"
+    private val ROOT_URI = "https://api.esv.org/v3/passage/text/"
+}
