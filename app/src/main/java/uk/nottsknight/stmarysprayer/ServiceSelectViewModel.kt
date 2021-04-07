5b4bd95a1475eeb54fe7bf51aa10e8b47a80c424
Ian Knight <ian.knight.1990@gmail.com>
2021-04-07 19:02:41 +0100

    Tries to start setting up viewmodel

--- app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectViewModel.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectViewModel.kt
@@ -3,5 +3,7 @@ package uk.nottsknight.stmarysprayer
 import androidx.lifecycle.ViewModel
 
 class ServiceSelectViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun onClickEnglishPrayerBook() {}
+
+    fun onClickCommonWorshipDailyPrayer() {}
 }
