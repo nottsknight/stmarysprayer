439dcc954843783b59120d6f62e4efde5c3d2c9c
Ian Knight <ian.knight.1990@gmail.com>
2021-04-08 17:58:27 +0100

    Correctly configures layout of settings fragment

--- app/src/main/java/uk/nottsknight/stmarysprayer/MainActivity.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/MainActivity.kt
@@ -1,7 +1,10 @@
 package uk.nottsknight.stmarysprayer
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,4 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_action_bar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.actionbar_settings -> findNavController(R.id.nav_host_fragment_container).navigate(
+                R.id.action_serviceSelectFragment_to_settingsFragment
+            ).let { true }
+            else -> false
+        }
 }
