7a65227f4d2e21ccdb0af961267adffab2e3e9d0
Ian Knight <ian.knight.1990@gmail.com>
2021-04-08 20:24:19 +0100

    Start filling in EPB service

--- app/src/main/java/uk/nottsknight/stmarysprayer/prayer/EnglishPrayerBookViewModel.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/prayer/EnglishPrayerBookViewModel.kt
@@ -1,12 +1,20 @@
 package uk.nottsknight.stmarysprayer.prayer
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import uk.nottsknight.stmarysprayer.R
 
-class EnglishPrayerBookViewModel : ViewModel() {
+class EnglishPrayerBookViewModel(private val app: Application) : AndroidViewModel(app) {
     val scriptureSentence1 = MutableLiveData<String>()
     val scriptureSentence2 = MutableLiveData<String>()
+    val confession = MutableLiveData<String>()
     val psalm = MutableLiveData<String>()
     val oldTestament = MutableLiveData<String>()
     val newTestament = MutableLiveData<String>()
+
+    fun initialiseTexts() {
+        app.resources.getStringArray(R.array.epb_confession_texts).random()
+            .let { confession.postValue(it) }
+    }
 }
