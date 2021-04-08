152ef933d0efff1ab2951aef991fc1725fa28732
Ian Knight <ian.knight.1990@gmail.com>
2021-04-08 17:16:59 +0100

    Issue 1 - ESV API (#2)
    * Create API data classes
    
    * Add dependency on Fuel
    
    * Empty EsvApi class
    
    * Implement basic EsvApi object
    
    * Fix a bad type in API response
    
    * Convert over to using Volley
    
    * Create EsvApi object to hide Volley internals
    
    * Implement preferences to control passage display
    
    * Remove an errant ampersand
    
    * Start providing tests for the API
    
    * Use SwitchPreferences

---
+++ app/src/main/java/uk/nottsknight/stmarysprayer/esvapi/EsvApiResponse.kt
@@ -0,0 +1,59 @@
+package uk.nottsknight.stmarysprayer.esvapi
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EsvApiResponse(
+    val query: String,
+    val canonical: String,
+    val parsed: Array<Array<Int>>,
+    @SerialName("passage_meta") val passageMeta: Array<EsvPassageMeta>,
+    val passages: Array<String>
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EsvApiResponse
+
+        if (query != other.query) return false
+        if (canonical != other.canonical) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = query.hashCode()
+        result = 31 * result + canonical.hashCode()
+        return result
+    }
+}
+
+@Serializable
+data class EsvPassageMeta(
+    val canonical: String,
+    @SerialName("chapter_start") val chapterStart: Array<Int>,
+    @SerialName("chapter_end") val chapterEnd: Array<Int>,
+    @SerialName("prev_verse") val prevVerse: Int?,
+    @SerialName("next_verse") val nextVerse: Int?,
+    @SerialName("prev_chapter") val prevChapter: Array<Int>?,
+    @SerialName("next_chapter") val nextChapter: Array<Int>?
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EsvPassageMeta
+
+        if (canonical != other.canonical) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return canonical.hashCode()
+    }
+}
