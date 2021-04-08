18ef0e52d530b917276fe09462d7cd69e2461f4d
Ian Knight <ian.knight.1990@gmail.com>
2021-04-08 12:29:13 +0100

    Finish setting up data binding

--- app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectFragment.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import uk.nottsknight.stmarysprayer.databinding.FragmentServiceSelectBinding
 
 class ServiceSelectFragment : Fragment() {
 
@@ -13,19 +15,20 @@ class ServiceSelectFragment : Fragment() {
         fun newInstance() = ServiceSelectFragment()
     }
 
-    private lateinit var viewModel: ServiceSelectViewModel
+    private val viewModel: ServiceSelectViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_service_select, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ServiceSelectViewModel::class.java)
-        // TODO: Use the ViewModel
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentServiceSelectBinding>(
+            inflater,
+            R.layout.fragment_service_select,
+            container,
+            false
+        )
+        binding.viewModel = viewModel
+        return binding.root
     }
 
 }
