1ab78f6bfd4dc94f1127e3b28ff23598ff7f0404
Ian Knight <ian.knight.1990@gmail.com>
2021-04-09 21:59:00 +0100

    Ensures coroutines properly used to get scripture

--- app/src/main/java/uk/nottsknight/stmarysprayer/esvapi/EsvApiService.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/esvapi/EsvApiService.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.preference.PreferenceManager
 import arrow.core.Either
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -20,13 +22,13 @@ private const val ROOT_URI = "https://api.esv.org/v3/passage/"
 private interface EsvApiService {
     @Headers("Authorization: Token $API_KEY")
     @GET("text")
-    fun getPassagesText(
+    suspend fun getPassagesText(
         @Query("q") passages: String,
         @Query("include-verse-numbers") includeVerseNumbers: Boolean,
         @Query("include-first-verse-numbers") includeFirstVerseNumbers: Boolean,
         @Query("include-headings") includeHeadings: Boolean,
         @Query("include-footnotes") includeFootnotes: Boolean
-    ): Call<EsvApiResponse>
+    ): Response<EsvApiResponse>
 }
 
 @ExperimentalSerializationApi
@@ -40,35 +42,36 @@ class EsvApiController(private val context: Context) {
         retrofit.create(EsvApiService::class.java)
     }
 
-    fun getPassages(passages: List<String>): Either<Int, List<String>> {
-        val query =
-            passages.map { it.replace(whitespaceRegex, "") }.reduce { acc, s -> acc + s }
+    suspend fun getPassages(passages: List<String>): Either<Int, List<String>> =
+        withContext(Dispatchers.IO) {
+            val query = passages
+                .map { it.replace(whitespaceRegex, "") }
+                .reduce { acc, s -> acc + s }
 
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val verseNumbers = preferences.getBoolean(
-            context.getString(R.string.prefs_scripture_versenumbers_key),
-            true
-        )
-        val firstVerseNumbers = preferences.getBoolean(
-            context.getString(R.string.prefs_scripture_firstversenumbers_key),
-            true
-        )
-        val headings = preferences.getBoolean(
-            context.getString(R.string.prefs_scripture_headings_key),
-            true
-        )
-        val footnotes = preferences.getBoolean(
-            context.getString(R.string.prefs_scripture_footnotes_key),
-            true
-        )
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val verseNumbers = preferences.getBoolean(
+                context.getString(R.string.prefs_scripture_versenumbers_key),
+                true
+            )
+            val firstVerseNumbers = preferences.getBoolean(
+                context.getString(R.string.prefs_scripture_firstversenumbers_key),
+                true
+            )
+            val headings = preferences.getBoolean(
+                context.getString(R.string.prefs_scripture_headings_key),
+                true
+            )
+            val footnotes = preferences.getBoolean(
+                context.getString(R.string.prefs_scripture_footnotes_key),
+                true
+            )
 
-        return service.getPassagesText(query, verseNumbers, firstVerseNumbers, headings, footnotes)
-            .execute().let {
-                if (it.isSuccessful) {
-                    Either.Right(it.body()?.passages?.toList() ?: listOf())
-                } else {
-                    Either.Left(it.code())
-                }
+            val response =
+                service.getPassagesText(query, verseNumbers, firstVerseNumbers, headings, footnotes)
+            if (response.isSuccessful) {
+                Either.Right(response.body()?.passages?.asList() ?: listOf())
+            } else {
+                Either.Left(response.code())
             }
-    }
+        }
 }
