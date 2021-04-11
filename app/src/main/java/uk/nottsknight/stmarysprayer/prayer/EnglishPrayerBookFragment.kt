0f43347d58498e6683fc25253f5cd117de17dde5
Ian Knight <ian.knight.1990@gmail.com>
2021-04-11 17:01:34 +0100

    Issue 5 - English Prayer Book (#11)
    * Basic setup for EPB fragment/viewmodel
    
    * Start filling in EPB service
    
    * Incorporate ESV API to get EPB passages
    
    * Create scripture livedata
    
    * Ensures coroutines properly used to get scripture
    
    * Add confession text livedata
    
    * Data binding works now!
    
    * Ensure text views fill the parent
    
    * Completes text for preparation and word of god
    
    * Place scrollview in fragment instead of activity
    
    * Implement error message toast
    
    * Adds prayers and collects
    
    * Finishes out EPB texts
    
    * Fix scrolling layout

--- app/src/main/java/uk/nottsknight/stmarysprayer/prayer/EnglishPrayerBookFragment.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/prayer/EnglishPrayerBookFragment.kt
@@ -1,32 +1,40 @@
 package uk.nottsknight.stmarysprayer.prayer
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import kotlinx.serialization.ExperimentalSerializationApi
 import uk.nottsknight.stmarysprayer.R
+import uk.nottsknight.stmarysprayer.databinding.FragmentEnglishPrayerBookBinding
 
+@ExperimentalSerializationApi
 class EnglishPrayerBookFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = EnglishPrayerBookFragment()
-    }
-
-    private lateinit var viewModel: EnglishPrayerBookViewModel
+    private val viewModel: EnglishPrayerBookViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_english_prayer_book, container, false)
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentEnglishPrayerBookBinding>(
+            inflater,
+            R.layout.fragment_english_prayer_book,
+            container,
+            false
+        )
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(EnglishPrayerBookViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.errorMessage.observe(viewLifecycleOwner) {
+            Toast.makeText(this.context, it, Toast.LENGTH_SHORT).show()
+        }
     }
-
 }
--- app/src/main/java/uk/nottsknight/stmarysprayer/prayer/EnglishPrayerBookViewModel.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/prayer/EnglishPrayerBookViewModel.kt
@@ -1,11 +1,112 @@
 package uk.nottsknight.stmarysprayer.prayer
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.liveData
+import arrow.core.Either
+import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.ExperimentalSerializationApi
+import uk.nottsknight.stmarysprayer.R
 import uk.nottsknight.stmarysprayer.esvapi.EsvApiController
+import java.util.*
 
+@ExperimentalSerializationApi
 class EnglishPrayerBookViewModel(app: Application) : AndroidViewModel(app) {
-    @ExperimentalSerializationApi
-    private val api: EsvApiController by lazy { EsvApiController(app.baseContext) }
+    private val api: EsvApiController = EsvApiController(app.baseContext)
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> get() = _errorMessage
+
+    private fun makeErrorMessage(code: Int) = "Failed to download passage (error $code)"
+
+    val scriptureSentence1 = liveData(Dispatchers.IO) {
+        Log.d("smwp", "scriptureSentence1 liveData")
+        when (val passages = api.getPassages(listOf(SCRIPTURE_SENTENCES.random()))) {
+            is Either.Right -> emit(passages.value[0])
+            is Either.Left -> _errorMessage.postValue(makeErrorMessage(passages.value))
+        }
+    }
+
+    val scriptureSentence2 = liveData(Dispatchers.IO) {
+        Log.d("smwp", "scriptureSentence2 liveData")
+        when (val passages = api.getPassages(listOf(SCRIPTURE_SENTENCES.random()))) {
+            is Either.Right -> emit(passages.value[0])
+            is Either.Left -> _errorMessage.postValue(makeErrorMessage(passages.value))
+        }
+    }
+
+    val confession = liveData {
+        Log.d("smwp", "confession liveData")
+        val confession = app.resources.getStringArray(R.array.epb_confession_texts).random()
+        emit(confession)
+    }
+
+    val psalm = liveData(Dispatchers.IO) {
+        Log.d("smwp", "psalm liveData")
+        when (val passages = api.getPassages(listOf("Psalm 95"))) {
+            is Either.Right -> emit(passages.value[0])
+            is Either.Left -> _errorMessage.postValue(makeErrorMessage(passages.value))
+        }
+    }
+
+    val oldTestament = liveData(Dispatchers.IO) {
+        Log.d("smwp", "oldTestament liveData")
+        when (val passages = api.getPassages(listOf("Genesis 1"))) {
+            is Either.Right -> emit(passages.value[0])
+            is Either.Left -> _errorMessage.postValue(makeErrorMessage(passages.value))
+        }
+    }
+
+    val newTestament = liveData(Dispatchers.IO) {
+        Log.d("smwp", "newTestament liveData")
+        when (val passages = api.getPassages(listOf("Matthew 1"))) {
+            is Either.Right -> emit(passages.value[0])
+            is Either.Left -> _errorMessage.postValue(makeErrorMessage(passages.value))
+        }
+    }
+
+    val collect = liveData(Dispatchers.IO) {
+        val collectArray = when (Calendar.getInstance()[Calendar.AM_PM]) {
+            Calendar.AM -> R.array.epb_morning_collects
+            Calendar.PM -> R.array.epb_evening_collects
+            else -> 0
+        }
+
+        if (collectArray != 0) {
+            app.resources.getStringArray(collectArray).random().let { emit(it) }
+        } else {
+            _errorMessage.postValue("Could not determine time of day")
+        }
+    }
+
+    val blessing = liveData(Dispatchers.IO) {
+        app.resources.getStringArray(R.array.epb_conclusion_blessings).random().let { emit(it) }
+    }
+
+    companion object {
+        private val SCRIPTURE_SENTENCES = arrayOf(
+            "Psalm 46:1",
+            "Psalm 51:3",
+            "Psalm 51:9",
+            "Psalm 51:17",
+            "Psalm 118:24",
+            "Psalm 143:2",
+            "Isaiah 55:6",
+            "Jeremiah 10:24",
+            "Ezekiel 18:27",
+            "Daniel 9:9",
+            "Joel 2:13",
+            "Habakkuk 2:20",
+            "Matthew 3:2",
+            "Luke 15:18",
+            "John 4:23",
+            "Philippians 4:6",
+            "Hebrews 13:15",
+            "1 John 1:8-9",
+            "Revelation 4:11"
+        )
+    }
 }
