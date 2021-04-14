1440fac33e3e6f546f6193ae0443ba01988110c3
Ian Knight <ian.knight.1990@gmail.com>
2021-04-14 11:39:07 +0100

    Switches to ListView

--- app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectFragment.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectFragment.kt
@@ -4,24 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import uk.nottsknight.stmarysprayer.databinding.FragmentServiceSelectBinding
 
-class ServiceSelectFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ServiceSelectFragment()
-    }
-
+class ServiceSelectFragment : ListFragment() {
     private val viewModel: ServiceSelectViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // data binding
         val binding = DataBindingUtil.inflate<FragmentServiceSelectBinding>(
             inflater,
             R.layout.fragment_service_select,
@@ -36,6 +34,22 @@ class ServiceSelectFragment : Fragment() {
         }
         binding.viewModel = viewModel
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listAdapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                context?.resources?.getStringArray(R.array.sselect_services) ?: arrayOf()
+            )
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) = when (position) {
+        0 -> findNavController().navigate(R.id.action_serviceSelectFragment_to_englishPrayerBookFragment)
+        1 -> findNavController().navigate(R.id.action_serviceSelectFragment_to_commonWorshipFragment)
+        else -> Unit
     }
 }
 
