9f0538d0dbfbc7a7db57e1826081cf6d79133bb7
Ian Knight <ian.knight.1990@gmail.com>
2021-04-14 11:50:12 +0100

    Issue 8 - Services list (#21)
    * Switches to ListView
    
    * Remove unnecessary code

--- app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectFragment.kt
+++ app/src/main/java/uk/nottsknight/stmarysprayer/ServiceSelectFragment.kt
@@ -1,44 +1,26 @@
 package uk.nottsknight.stmarysprayer
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.ListFragment
 import androidx.navigation.fragment.findNavController
-import uk.nottsknight.stmarysprayer.databinding.FragmentServiceSelectBinding
 
-class ServiceSelectFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ServiceSelectFragment()
+class ServiceSelectFragment : ListFragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listAdapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                context?.resources?.getStringArray(R.array.sselect_services) ?: arrayOf()
+            )
     }
 
-    private val viewModel: ServiceSelectViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val binding = DataBindingUtil.inflate<FragmentServiceSelectBinding>(
-            inflater,
-            R.layout.fragment_service_select,
-            container,
-            false
-        )
-        viewModel.englishPrayerBookCallback = NavigationCallback {
-            findNavController().navigate(R.id.action_serviceSelectFragment_to_englishPrayerBookFragment)
-        }
-        viewModel.commonWorshipCallback = NavigationCallback {
-            findNavController().navigate(R.id.action_serviceSelectFragment_to_commonWorshipFragment)
-        }
-        binding.viewModel = viewModel
-        return binding.root
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) = when (position) {
+        0 -> findNavController().navigate(R.id.action_serviceSelectFragment_to_englishPrayerBookFragment)
+        1 -> findNavController().navigate(R.id.action_serviceSelectFragment_to_commonWorshipFragment)
+        else -> Unit
     }
-}
-
-fun interface NavigationCallback {
-    fun performNavigation()
 }
